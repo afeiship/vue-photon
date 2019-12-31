9ce4ba28617352b7bf0000007c3b917f9156d58c
afeiship <1290657123@qq.com>
2019-12-31 21:20:05 +0800

    add: btn basic example

--- packages/button/index.vue
+++ packages/button/index.vue
@@ -3,7 +3,7 @@
     :class="[
       'btn',
       theme ? `btn-${theme}` : '',
-      size ? size : '',
+      size ? `btn-${size}` : '',
       {
         active
       }
@@ -19,7 +19,7 @@ export default {
   props: {
     theme: {
       type: String,
-      default: 'default'
+      default: ''
     },
     size: {
       type: String,
--- public/components/button/index.vue
+++ public/components/button/index.vue
@@ -1,7 +1,37 @@
 <template>
-  <div>
-    <vp-button>Primary</vp-button>
-    <vp-button theme="positive">Positive</vp-button>
-    <vp-button theme="warning">Warning</vp-button>
+  <div class="padded container">
+    <div class="is-row">
+      <vp-button
+        size="large"
+        v-for="(item, index) in themes"
+        :key="index"
+        :theme="item"
+        >{{ item }}</vp-button
+      >
+    </div>
+    <div class="is-row">
+      <vp-button v-for="(item, index) in themes" :key="index" :theme="item">{{
+        item
+      }}</vp-button>
+    </div>
+    <div class="is-row">
+      <vp-button
+        size="mini"
+        v-for="(item, index) in themes"
+        :key="index"
+        :theme="item"
+        >{{ item }}</vp-button
+      >
+    </div>
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      themes: require('@/public/assets/json/theme.json'),
+      sizes: require('@/public/assets/json/size.json')
+    };
+  }
+};
+</script>
