1b3dd4d026c77dc395afd17c5aef6fab23808a50
afeiship <1290657123@qq.com>
2020-01-03 11:26:28 +0800

    add: basic table

---
+++ packages/table/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div :class="['table', { 'table-striped': striped }]">
+    <thead>
+      <tr>
+        <template v-for="(item, index) in columns">
+          <slot name="column" v-bind="{ item, index }" />
+        </template>
+      </tr>
+    </thead>
+
+    <tbody>
+      <tr v-for="(item, index) in items" :key="index">
+        <td v-for="column in columns" :key="column.dataIndex">
+          <slot name="item" v-bind="{ item, column, index }" />
+        </td>
+      </tr>
+    </tbody>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'vp-table',
+  props: {
+    striped: Boolean,
+    columns: Array,
+    items: Array
+  }
+};
+</script>
---
+++ public/components/table/index.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="padded container">
+    <vp-table :items="table.items" :columns="table.columns">
+      <template v-slot:column="{ item, index }">
+        <th>{{ item.label }}</th>
+      </template>
+
+      <template v-slot:item="{ item, column, index }">
+        <td>{{ item[column.dataIndex] }}</td>
+      </template>
+    </vp-table>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      table: {
+        columns: [
+          { key: 'name', label: 'Name', dataIndex: 'name' },
+          { key: 'kind', label: 'Kind', dataIndex: 'kind' },
+          { key: 'date', label: 'Date Modified', dataIndex: 'date' },
+          { key: 'author', label: 'Author', dataIndex: 'author' }
+        ],
+        items: [
+          {
+            id: 'bars.scss',
+            name: 'bars.scss',
+            kind: 'Document',
+            date: 'Oct 13, 2015',
+            author: 'connors'
+          },
+          {
+            id: 'buttons.scss',
+            name: 'buttons.scss',
+            kind: 'Document',
+            date: 'Oct 13, 2015',
+            author: 'afei'
+          },
+          {
+            id: 'lists.scss',
+            name: 'lists.scss',
+            kind: 'Document',
+            date: 'Oct 13, 2015',
+            author: 'afeiship'
+          }
+        ]
+      }
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.container {
+  color: #333;
+}
+</style>
