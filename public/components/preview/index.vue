35b0648b4be49cff6c6a4386bb1609ea96a7cddf
afeiship <1290657123@qq.com>
2020-01-09 23:59:31 +0800

    add: preview page

--- public/components/preview/index.vue
+++ public/components/preview/index.vue
@@ -1,25 +1,26 @@
-<template>
-  <vp-container>
-    <template v-for="item in components">
-      {{ item }}
-    </template>
-  </vp-container>
-</template>
-
 <script>
 export default {
   data() {
     return {
-      items: require('@/public/assets/json/routes.json'),
+      items: require('@/public/assets/json/routes.json')
     };
   },
-  computed:{
-    components(){
-      const items = this.items;
-      return items.map(item=>{
-        return require(`@/public/components/${item}`).default
-      })
+  computed: {
+    components() {
+      const items = this.items.filter((item) => item !== 'preview');
+      return items.map((item) => {
+        return require(`@/public/components/${item}`);
+      });
     }
+  },
+  render(createElement) {
+    return createElement(
+      'vp-container',
+      {},
+      this.components.map((item) => {
+        return createElement(item.default);
+      })
+    );
   }
 };
 </script>
